98d78d4b509fcec1f475d672c8550e71797f2698
baozx <bzx0574@126.com>
2020-11-18 14:34:43 +0800

    fix: 正确的做法

--- exam.cpp
+++ exam.cpp
@@ -9,21 +9,13 @@ using namespace std;
 * 如果temp大则查在该元素后面
 */
 void InsertElem(int a[], int m, int n) {
-	//int i, j, temp;
-	//for (i = m; i < m + n; ++i) {
-	//	temp = a[i];
-	//	for (j = i - 1; j >= 0; --j) {
-	//		if (temp < a[j]) {
-	//			a[j + 1] = a[j];
-	//		}
-	//	}
-	//	a[j + 1] = temp;
-	//}
-	int i, j;
-	int temp;
+	int i, j, temp;
 	for (i = m; i < m + n; ++i) {
 		temp = a[i];
-		for (j = i - 1; j >= 0 && temp < a[j]; --j) {
+		for (j = i - 1; j >= 0; --j) {
+			if (temp > a[j]) {
+				break;
+			}
 			a[j + 1] = a[j];
 		}
 		a[j + 1] = temp;
@@ -39,11 +31,10 @@ void PrintArray(int a[], int length) {
 int main() {
 	int m, n;
 	m = 5;
-	n = 4;
-	int a[] = { 1,3,5,7,9,2,4,6,8 };
+	n = 2;
+	int a[] = { 1,3,5,7,9,2,4};
 	PrintArray(a, 9);
-	InsertElem(a, 5, 4);
+	InsertElem(a, 5, 2);
 	PrintArray(a, 9);
-
 	return 0;
 }
